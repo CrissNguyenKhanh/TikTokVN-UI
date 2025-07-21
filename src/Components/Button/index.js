2c280f3896fd2b205b18b06fe77277f416c4ce70
CrissNguyenKhanh <quockhanhdz295@gmail.com>
2025-07-21 16:17:11 +0700

    FontSameToptop

--- src/Components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import handleAlert from "~/myAlert";
-
-function Button() {
-  return <div onClick={handleAlert}>Click Me!</div>;
-}
-
-export default Button;
--- src/myAlert.js
+++
@@ -1,5 +0,0 @@
-const handleAlert = () => {
-  alert("say Hi!");
-};
-
-export default handleAlert;
