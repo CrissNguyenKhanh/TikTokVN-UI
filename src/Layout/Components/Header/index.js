de1a1f0f547033f62f50059ad3a2c7328d70b7f3
CrissNguyenKhanh <quockhanhdz295@gmail.com>
2025-07-22 21:37:26 +0700

    Header-uiReal

--- src/Layout/Components/Header/index.js
+++ src/Layout/Components/Header/index.js
@@ -1,15 +1,33 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './header.module.scss';
+import images from '~/src/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
-        <h2 className={cx('wrapper')}>
+        <header className={cx('wrapper')}>
+            s{' '}
             <div className={cx('inner')}>
-                
+                <img src={images.logo} alt="Tiktok" />
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}></div>
             </div>
-        </h2>
+        </header>
     );
 }
+
 export default Header;
--- src/Layout/DefaultLayout/index.js
+++ src/Layout/DefaultLayout/index.js
@@ -1,7 +1,7 @@
 import classNames from 'classnames/bind';
 import styles from './defaultlayout.module.scss';
-import Header from '../Components/Header';
 import SideBar from './SiderBar';
+import Header from '../Components/Header';
 
 const cx = classNames.bind(styles);
 
